1da8d390136bfae26184973cccf60f29b528238c
Valentin Mathey <estudiovalentinmathey@gmail.com>
2023-04-13 21:57:40 -0300

    Project Basics

---
+++ src/main/java/com/egg/MiMaridoTeLoHace/Entities/Customer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.egg.MiMaridoTeLoHace.Entities;
+
+/**
+ *
+ * @author mathe
+ */
+public class Customer {
+    
+}
