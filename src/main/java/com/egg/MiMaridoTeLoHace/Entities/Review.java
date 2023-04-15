a31be7bc9160eb85c5ec5b53c4b9f5d318d97ddb
Valentin Mathey <estudiovalentinmathey@gmail.com>
2023-04-15 10:46:53 -0300

    optimizing with lombok and solving problems

--- src/main/java/com/egg/MiMaridoTeLoHace/Entities/Review.java
+++ src/main/java/com/egg/MiMaridoTeLoHace/Entities/Review.java
@@ -6,61 +6,23 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
+@Data
 @Entity
 public class Review {
     
     @Id
     @GeneratedValue (generator = "uuid")
+    @GenericGenerator (name = "uuid", strategy = "uuid2")
     private String id;
 
     private String comment;
-    private Provider provider;
-    private Customer costumer;
+//    private Provider provider;
+//    private Customer costumer;
 
     @Enumerated(EnumType.STRING)
     private Ratings rating;
 
-    public Review() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Provider getProvider() {
-        return provider;
-    }
-
-    public void setProvider(Provider provider) {
-        this.provider = provider;
-    }
-
-    public Customer getCostumer() {
-        return costumer;
-    }
-
-    public void setCostumer(Customer costumer) {
-        this.costumer = costumer;
-    }
-
-    public Ratings getRating() {
-        return rating;
-    }
-
-    public void setRating(Ratings rating) {
-        this.rating = rating;
-    }
 }
