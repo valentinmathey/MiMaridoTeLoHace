ff5d246892ce469f614e72c43cd4f09a886b0ae0
“fnedic” <facunedic@gmail.com>
2023-04-19 17:00:53 -0300

    admin controller and exceptions correction

--- src/main/java/com/egg/MiMaridoTeLoHace/Controllers/AdminController.java
+++ src/main/java/com/egg/MiMaridoTeLoHace/Controllers/AdminController.java
@@ -1,14 +1,23 @@
 package com.egg.MiMaridoTeLoHace.Controllers;
 
+import com.egg.MiMaridoTeLoHace.Entities.Admin;
+import com.egg.MiMaridoTeLoHace.Exceptions.MiException;
 import com.egg.MiMaridoTeLoHace.Services.AdminService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/Admin")
+@RequestMapping("/admin")
 public class AdminController {
     @Autowired
     AdminService adminService;
@@ -19,5 +28,60 @@ public class AdminController {
         return "index.html";
     }
 
+    @GetMapping("/create")
+    public String create(@RequestBody Admin admin) throws MiException{
+
+        try {
+
+            adminService.createAdmin(admin);
+            return "index.html";
+
+        } catch (Exception e) {
+            throw new MiException("Error al crear Admin");
+        }
+    }
+
+    @PutMapping("/modify/{id}")
+    public String modify(@PathVariable("id") String id, @RequestBody Admin admin) throws MiException{
+
+        try {
+            
+            Admin modifiedAdmin = adminService.searchById(id);
+            modifiedAdmin.setName(admin.getName());
+            modifiedAdmin.setEmail(admin.getEmail());
+            adminService.modifyAdmin(modifiedAdmin);
+
+            return "index.html";
+
+        } catch (Exception e) {
+            throw new MiException("ERROR al modificar Administrador");
+        }
+
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable("id") String id) throws MiException{
+
+        try {
+            
+            adminService.deleteAdmin(id);
+            return "index.htm";
+
+        } catch (Exception e) {
+            throw new MiException("Admin no encontrado!");
+        }
+
+    }
     
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        
+        List<Admin> adminList = adminService.listAdmins();
+        model.addAttribute("admins", adminList);
+
+        return "autor_list.hmtl";
+        //el nombre este ultimo HTML se puede cambiar, asi como podemos prescindir de este bloque de codigo si es que no queremos listar admins
+    }
+
 }
--- src/main/java/com/egg/MiMaridoTeLoHace/Controllers/CustomerController.java
+++ src/main/java/com/egg/MiMaridoTeLoHace/Controllers/CustomerController.java
@@ -9,38 +9,38 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/Customer")
+@RequestMapping("/customer")
 public class CustomerController {
     @Autowired
     CustomerService customerServices;
 
     @GetMapping("user/{id}")
-    public String Perfil(@PathVariable("id") String id, ModelMap model){
-        model.addAttribute(customerServices.searchBychId(id));
-        return "user";
+    public String perfil(@PathVariable("id") String id, ModelMap model){
+        model.addAttribute(customerServices.searchById(id));
+        return "user.html";
     }
     
     @GetMapping("create")
-    public String Create(@RequestBody Customer customer) throws MiException {
+    public String create(@RequestBody Customer customer) throws MiException {
         customerServices.createCustomer(customer);
-        return "index";
+        return "index.html";
     }
     
     @DeleteMapping("delete/{id}")
-    public String Delete(@PathVariable("id") String id){
+    public String delete(@PathVariable("id") String id) throws MiException{
         customerServices.deleteCustomer(id);
-        return "index";
+        return "index.html";
     }
     
     @PutMapping("modify/{id}")
-    public String Modify(@PathVariable("id") String id, @RequestBody Customer customer) throws MiException {
-        Customer customerModify = customerServices.searchBychId(id);
+    public String modify(@PathVariable("id") String id, @RequestBody Customer customer) throws MiException {
+        Customer customerModify = customerServices.searchById(id);
         customerModify.setName(customer.getName());
         customerModify.setLocation(customer.getLocation());
         customerModify.setEmail(customer.getEmail());
 
         customerServices.modifyCustomer(customerModify);
-        return "index";
+        return "index.html";
     }
 
 
