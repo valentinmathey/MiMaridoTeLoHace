7c4bfffe72dc1cf0f7e6218e17f5cc76be10796b
eric Lemos <Eric.e.lemos0220@gmail.com>
2023-05-03 15:38:09 -0300

    WorkController WorkService

--- src/main/java/com/egg/MiMaridoTeLoHace/Services/WorkService.java
+++ src/main/java/com/egg/MiMaridoTeLoHace/Services/WorkService.java
@@ -3,39 +3,55 @@ package com.egg.MiMaridoTeLoHace.Services;
 import com.egg.MiMaridoTeLoHace.Entities.Work;
 import com.egg.MiMaridoTeLoHace.Entities.User;
 import com.egg.MiMaridoTeLoHace.Enums.Roles;
-import com.egg.MiMaridoTeLoHace.Exceptions.MiException;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.egg.MiMaridoTeLoHace.Repositories.WorkRepository;
 
 @Service
 public class WorkService {
-    //pruebas con reviews
+    // pruebas con reviews
 
     @Autowired
     WorkRepository workRepository;
 
     @Autowired
     UserService userService;
-    
-    public void createWork(User customer, User provider, Work workSave) throws MiException {
-        
 
+    @Transactional
+    public void createWork(Work work) {
+        // les agrega al customer y al provider el trabajo como pendiente (si es que se
+        // quiere eso)
+        userService.setWorkPendiente(work);
+
+        workRepository.save(work);
     }
-    
-    public void createReview(String Id, Work workReview) throws MiException {
 
+    @Transactional
+    public void editReview(Work work) {
+        Work original = getById(work.getId());
 
-    }
-
-    //tanto provider como user puede consultar sus review
-    public void searchWork(User user){
-        //eric: no se por que estan separados, pero puedo pensar que solo para que el customer pueda editar los comentarios?
-        if (user.getRole().equals(Roles.PROVIDER)){
-            workRepository.getWorkByUserProvider(user.getId());
-        }else if (user.getRole().equals(Roles.CUSTOMER)){
-            workRepository.getWorkByUserCustomer(user.getId());
+        if (original.getWorkFinished()) {
+            original.setRatingWork(work.getRatingWork());
+            original.setReview(work.getReview());
+        } else {
+            if (work.getWorkFinished()) {
+                original.setWorkFinished(work.getWorkFinished());
+            } else if (work.getWorkStarted()) {
+                original.setWorkStarted(work.getWorkStarted());
+            }
         }
+        workRepository.save(original);
+    }
 
+    @Transactional
+    public void delete(String id) {
+        workRepository.delete(getById(id));
+    }
+
+    public Work getById(String id) {
+        return workRepository.findById(id).get();
     }
 }
