b326c8e607f9c3ed4adc83e1d6a62ac513d956e8
Valentin Mathey <estudiovalentinmathey@gmail.com>
2023-05-03 12:09:14 -0300

    Add class work and html

---
+++ src/main/java/com/egg/MiMaridoTeLoHace/Repositories/WorkRepository.java
@@ -0,0 +1,16 @@
+package com.egg.MiMaridoTeLoHace.Repositories;
+
+import com.egg.MiMaridoTeLoHace.Entities.Work;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+public interface WorkRepository extends JpaRepository<Work, String> {
+
+    @Query("SELECT w FROM Work w WHERE w.userProviderId =: provider_id")
+    List<Work> getWorkByUserProvider(@Param("provider_id") String provider);
+
+    @Query("SELECT w FROM Work w WHERE w.userCustomerId =: customer_id")
+    List<Work> getWorkByUserCustomer(@Param("customer_id") String customer);
+}
